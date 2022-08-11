787c90568aa2c591da24e5f36f72f7ae79b0f9ce
KaterynaVelichko <107834200+KaterynaVelichko@users.noreply.github.com>
2022-08-12 00:30:03 +0300

    Delete portfolio directory

--- portfolio/script/script.js
+++
@@ -1,5 +0,0 @@
-$('.about_items > div').on('click', function(){
-    $(this).children('.about_icon').toggleClass('about-rotate-icon');
-    $(this).next('.about_text').slideToggle(300);
-    $(this).parent('.about_items').toggleClass('change-color');
-})
